7c3401121773d377e9fd042b9fa6318feaa8b39c
Shaun Cheng <cheng754@gmail.com>
2025-09-15 23:26:43 -0700

    implement trie start.

---
+++ 0208-implement-trie-medium/main.c
@@ -0,0 +1,51 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+/*
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+ 
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+*/
+
+struct Node {
+    char c;
+    struct Node* next;
+};
+
+typedef struct {
+    struct Node node[26];
+
+
+} Trie;
+
+Trie* trieCreate() { return NULL; }
+
+void trieInsert(Trie* obj, char* word) {}
+
+bool trieSearch(Trie* obj, char* word) { return true; }
+
+bool trieStartsWith(Trie* obj, char* prefix) { return true; }
+
+void trieFree(Trie* obj) {}
+
+int main() {
+    printf("0208 implement trie!\n");
+    return 0;
+}
